6c177425aaa60d253a1688f997c04c962ee78d6f
Prasidh Gopal Anchan <92362239+Kawaki22@users.noreply.github.com>
2023-06-28 15:44:13 +0530

    Minor bug fixes

--- app/src/main/java/com/shoppy/shopkart/ShopKartUtils.kt
+++ app/src/main/java/com/shoppy/shopkart/ShopKartUtils.kt
@@ -8,20 +8,18 @@ import androidx.compose.ui.graphics.Color
 object ShopKartUtils{
 
     //Colors
-//    val offWhite = Color(0xFFF8F6F6)
     val offWhite = Color(0xFFF5F5F5)
     val blue = Color(0xFF5588FF)
     val darkBlue = Color(0xFF1C0927)
     const val black = 0xFF23272a
 
-    const val webClientId = "75755133388-rc7nfobjf70i2kk7pgunapfke5cf39mt.apps.googleusercontent.com"
+    const val webClientId = ""
 
     //FCM constants
-    const val API_KEY = "AIzaSyC9bCetIXBbWLSHQ90n0unX2xRFxKIydFw"
-    const val SERVER_KEY = "AAAAEaNbmcw:APA91bEMNIttOuq2EsbEeCDbqJiyGsu2l2263FjCiuUFEwWofhkIH7naic_bXnTXWWbSA_hcAE5Ymxck54GCrJFCJsZnbBifzRJfbjTfRBENLHwOHTUnPAMyC_FQdpzQkVBYChQlolD9"
+    const val API_KEY = ""
+    const val SERVER_KEY = ""
     const val BASE_URL = "https://fcm.googleapis.com/"
     const val CONTENT_TYPE = "application/json"
     val TOPIC = if (VERSION.SDK_INT >= VERSION_CODES.S) "Delivery Status" else "/topics/Delivery Status"
-
-//    const val RZP_API_KEY = "rzp_test_YQFWpJM7HeMRZ9"
+    const val RZP_API_KEY = ""
 }
